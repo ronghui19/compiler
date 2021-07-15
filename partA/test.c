7abc3e43e881e53d0a0fed323d44b7b8b56b5c9d
ronghui19 <171547022@qq.com>
2021-07-15 20:56:36 +0800

    update files

--- partA/test.c
+++ partA/test.c
@@ -7,69 +7,32 @@
 
 int main(int argc, char const *argv[])
 {
-    // char ch;
-    // FILE *fp = fopen("util.c", "r");
-    // ch = fgetc(fp);
 
-    // while ( (ch = fgetc(fp)) != EOF )
-    // {
-    //     putchar(ch);
-    //     putchar('\n');
-    // }
-    
-    // fclose(fp);
-
-
-    // FILE *fp;
-    // int len;
-    // int nProject = 0;
-
-    // fp = fopen("util.c", "r");
-    // if( fp == NULL )
-    // {
-    //     perror ("打开文件错误");
-    //     return(-1);
-    // }
-    // fseek(fp, 0, SEEK_END);
-
-    // len = ftell(fp);
-    // // char *buffer = malloc(sizeof(char)*len+1);
-    // char buffer[10000];
-    // buffer[nProject] = fgetc(fp); // init
-    // while (buffer[nProject] != '$') {
-    //     nProject++;
-    //     buffer[nProject] = fgetc(fp);
-    // }
-
-    // buffer[++nProject] = '\0';
-
-
-
-    // fclose(fp);
-
-    // printf("util.c 的总大小 = %d 字节\n", len);
-
-
-    // char buffer[10000];
     int len;
     int n = 0;//源程序指针
-
-    char *buffer = malloc(sizeof(char)*len+1);
-
     FILE *fp;
+
     if ((fp = fopen("util.c", "r")) == NULL)
     {//打开源程序
         perror ("file open error");
         exit(0);
     }
+    
+    fseek(fp, 0, SEEK_END);
+    len = ftell(fp);
+
+    char *buffer = malloc(sizeof(char)*len+1);
     buffer[n] = fgetc(fp);
+
     while((buffer[n] = fgetc(fp)) != EOF){
         n++;
-        if(n + 1 >= 10000){
+        if(n + 1 >= len){
             break;
         }
     }
+    
     buffer[n] = '\0';
+    printf("util.c 的总大小 = %d 字节\n", len);
     printf("%s\n", buffer);
     fclose(fp);
     free(buffer);
